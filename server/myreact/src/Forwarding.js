1a199f535b50d1f1cbc001c49bb5607baf096c51
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2023-12-27 20:35:21 +0530

    Routing added

---
+++ server/myreact/src/Forwarding.js
@@ -0,0 +1,40 @@
+import Home from "./HomeV3.1";
+import "./Home.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import {Link, Switch, Route} from 'react-router-dom'
+
+
+function Forwarding(){
+    return(
+        <>
+            <nav className="navbar navbar-expand-lg" style={{"backgroundColor":"white"}}>
+                <div className="container-fluid">
+                    <Link to="/home" className="navbar-brand logoFont" style={{"color":"black"}}>FlyHigh</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        {/* <a className="nav-link active myfont" aria-current="page" href="" style={{"color":"black"}}>Login</a> */}
+                        <Link to="/login" className="nav-link myfont" style={{"color":"black"}}>Login</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/about" className="nav-link myfont" style={{"color":"black"}}>About</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/contact" className="nav-link myfont" style={{"color":"black"}}>Contact</Link>  
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/home" exact component={Home}/>
+            </Switch>
+        </>
+    );
+}
+
+export default Forwarding;
--- server/myreact/src/HomeV3.1.js
+++ server/myreact/src/HomeV3.1.js
@@ -9,6 +9,7 @@ import "../node_modules/react-datepicker/dist/react-datepicker.css"
 const port = process.env.REACT_APP_PORT_NO;
 const serverIp = process.env.REACT_APP_SERVER_IP;
 function Home() {
+    const [searchResult, setSearchResult] = useState(false);
     const serverUrl = `http://${serverIp}:${port}`;
     const [flights, setFilghts] = useState([]);
     const getFlights = ()=>{
@@ -21,42 +22,8 @@ function Home() {
         getFlights();
     }, []);
 
-
     return ( 
         <>
-            <nav className="navbar navbar-expand-lg" style={{"backgroundColor":"white"}}>
-                <div className="container-fluid">
-                    <a className="navbar-brand logoFont" href="" style={{"color":"black"}}>Fly High</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <a className="nav-link active myfont" aria-current="page" href="" style={{"color":"black"}}>Login</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link myfont" href="" style={{"color":"black"}}>About</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link myfont" href="" style={{"color":"black"}}>Services</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link myfont" href="" style={{"color":"black"}}>Contact</a>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-            {/* <div className="jumbo">
-                <img src="favicon.png" className="logoImg" alt="" />
-                <p className="myfont logoText">
-                    FlyHigh 
-                    Explore Flights 
-                    Login
-                    Contact Us
-                </p>
-            </div> */}
             <div style={{"textAlign":"center"}}>
                 <div className="searchDiv restDiv">
                     <div style={{"textAlign":"center"}}>
@@ -79,20 +46,47 @@ function Home() {
                         <DatePicker className="searchBox myfont" placeholderText="Select Departure Date"></DatePicker>
                        </div>
                         <br />
-                        <button className="LinkedInFreeTrail">SEARCH</button>
+                        <button className="LinkedInFreeTrail" onClick={setSearchResult}>SEARCH</button>
                     </div>
                 </div>
             </div>
-            <div className="gridDiv">
-                <div className="growCity">
-                    <img className="cityImage" src="Bangalore.webp" alt="" />
-                    <img className="cityImage" src="Delhi.jpg" alt="" />
-                    <img className="cityImage" src="Indore.jpg" alt="" />
-                    <img className="cityImage" src="Pune.jpg" alt="" />
-                    <img className="cityImage" src="Jaipur.jpg" alt="" />
-                    <img className="cityImage" src="Mumbai.webp" alt="" />
-                </div>
-            </div>
+            {
+                searchResult &&
+                <>
+                    {flights.map((flight)=>{
+                        return (
+                            <>
+                                <div className="container">
+                                    <div className="row restDiv growDiv" key={flight}>
+                                        <div className="col"> <p className="myfont textVertical">{flight.name}</p></div> {" "}
+                                        <div className="col"> <p className="myfont textVertical">{flight.class}</p></div> {" "}
+                                        <div className="col"> <p className="myfont textVertical">{flight.destination}</p></div> {" "}
+                                        <div className="col"> <p className="myfont textVertical">{flight.source}</p></div> {" "}
+                                        <div className="col"> <p className="myfont textVertical">{flight.departure}</p></div> {" "}
+                                        <div className="col"> <p className="myfont textVertical">{flight.arrival}</p></div> {" "}
+                                        <div className="col"><button className="tweet">View</button></div>
+                                    </div> 
+                                </div>
+                            </>
+                        );
+                    })}
+                </>
+            }
+            {
+                !searchResult &&
+                <>
+                    <div className="gridDiv">
+                        <div className="growCity">
+                            <img className="cityImage" src="Bangalore.webp" alt="" />
+                            <img className="cityImage" src="Delhi.jpg" alt="" />
+                            <img className="cityImage" src="Indore.jpg" alt="" />
+                            <img className="cityImage" src="Pune.jpg" alt="" />
+                            <img className="cityImage" src="Jaipur.jpg" alt="" />
+                            <img className="cityImage" src="Mumbai.webp" alt="" />
+                        </div>
+                    </div>
+                </>
+            }
         </>
      );
 }
