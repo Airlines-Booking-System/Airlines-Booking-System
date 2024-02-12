40f1ca8dedf19959e7cadb9d1c422120b256ed44
Sneharakshe <ayushrakshe2916@gmail.com>
2024-02-12 18:15:38 +0530

    entities updated

---
+++ SpringBoot Backend/Airlines_backend/src/main/java/com/app/controllers/AdminController.java
@@ -0,0 +1,36 @@
+// package com.app.controllers;
+
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+// @RestController
+// @RequestMapping("/admin")
+// public class AdminController {
+
+//     @GetMapping("path")
+//     public String viewAllUsers(@RequestParam String param) {
+//         return new String();
+//     }
+
+//     public String CreateFlight(){
+
+//     }
+    
+//     public String deleteFlight(){
+
+//     }
+//     public String editFlight(){
+
+//     }
+
+//     public String assignRoles(){
+
+//     }
+
+//     public String viewAllPayments(){}
+    
+    
+// }
