ed7851efe4d731871bafd1e0de88ec53fcfe7a3c
Prachi <prachigupta.ggits@gmail.com>
2024-02-12 16:55:38 +0530

    sql queries added

--- mysql.sql
+++ mysql.sql
@@ -19,3 +19,22 @@ insert into flight_details (id, name, flight_class, destination, source, departu
 (default, 'Air Asia', 'Economy', 'PNQ', 'IDR', '2023-12-12 12:12:00', '2023-12-12 15:12:00'),
 (default, 'Indigo', 'Economy', 'IDR', 'PNQ', '2023-12-12 12:12:00', '2023-12-12 15:12:00'),
 (default, 'Akasa', 'Economy', 'BOM', 'IND', '2023-12-12 12:12:00', '2023-12-12 15:12:00');
+
+insert into user_details (id,cpass,email,name,role) values (default,'Prachi123','pg@gmail.com','Prachi','USER');
+
+insert into address_details(pincode, city, state) values (482001,'Jabalpur', 'MP'),(483001,'Ujjain', 'MP'), (484001,'Pune', 'Maharastra');
+
+insert into  general_details (customer_id,aadhar, address,dob,gender,mobile_number,pincode_pincode) values(1,'AA1','MP','2000-02-26','female','9999955555', 482001);
+
+
+
+
+//insert into booking_details(id,duration,fare_price,seat_no,customner_id, flight_id, paymentid_id, flight id) valuies(default, 3, 1000.00,10,1,1,1),(default, 4, 2000.00,11,2,2,2);
+
+//insert into payment_Details(id,status,total_amount,booking_id_id) values (default, 'Pending', 1000.00, 1),(default, 'Successful', 2000.00, 2);
+
+
+
+
+
+insert into passanger_details(id,name,bid_id,customer_id) values(1,)
