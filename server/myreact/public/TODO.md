89d1180a8bc14a6a6358179eee66deeae43697f0
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2024-02-21 11:47:52 +0530

    added booked flight

---
+++ server/myreact/public/TODO.md
@@ -0,0 +1,9 @@
+# USER:
+## view booked flights
+
+# ADMIN
+## view all users and delete
+## create flight
+## view/delete flight
+## edit flight
+## all payments
---
+++ server/myreact/src/ViewBookedFlights.jsx
@@ -0,0 +1,9 @@
+function ViewBookedFlights(){
+    return(
+        <>
+            <h1>BOOKED FLIGHTS</h1>
+        </>
+    )
+}
+
+export default ViewBookedFlights;
