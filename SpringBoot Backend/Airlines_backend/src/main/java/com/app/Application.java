e6300ca7e587c09827637f7cf04bbbb4d275035f
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2024-02-23 17:53:57 +0530

    modified project details in pom.xml

--- SpringBoot Backend/Airlines_backend/src/main/java/com/app/Application.java
+++ SpringBoot Backend/Airlines_backend/src/main/java/com/app/Application.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 
 	public static void main(String[] args) {
@@ -33,12 +34,6 @@ public class Application {
 	public PasswordEncoder encode()
 	{
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public SecurityFilterChain myChain(HttpSecurity http) throws Exception{
-		http.cors();
-		return http.build();
 	}
 
 }
