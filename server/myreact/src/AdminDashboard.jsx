39c0b2f606b05b14b8d6bf5edb16041b73ee83ac
D4_80737_SagarKhatri <sagarkhatri.pro@gmail.com>
2024-02-23 15:27:49 +0530

    added images to admin dashboard

--- server/myreact/src/AdminDashboard.jsx
+++ server/myreact/src/AdminDashboard.jsx
@@ -9,18 +9,30 @@ function AdminDashboard(){
                     Admin Dashboard
                 </p>
                 <div className="centerText">
-                    <p className="logoFont restDiv centerText cityImage" style={{"display":"inline-block"}} onClick={()=>{
+
+                    <div className="gridDiv" style={{"display":"inline-block"}} onClick={()=>{
                         navigate("/ViewUsers")
-                    }}>View Users</p>
-                    <p className="logoFont restDiv centerText cityImage" style={{"display":"inline-block"}} onClick={()=>{
+                    }}>
+                        <p className="logoFont">View Users</p>
+                    <img src="viewUsers.jpg" className="cityImage" alt=""/></div>
+                    <div className="gridDiv" style={{"display":"inline-block"}} onClick={()=>{
                         navigate("/CreateFlight")
-                    }}>Create Flight</p>
-                    <p className="logoFont restDiv centerText cityImage" style={{"display":"inline-block"}} onClick={()=>{
+                    }}>
+                        <p className="logoFont">Create Flight</p>
+                        <img src="createFlight.jpg" className="cityImage" alt="" />
+                    </div>
+                    <div className="gridDiv" style={{"display":"inline-block"}} onClick={()=>{
                         navigate("/ViewFlights")
-                    }}>View Flights</p>
-                    <p className="logoFont restDiv centerText cityImage" style={{"display":"inline-block"}} onClick={()=>{
+                    }}>
+                        <p className="logoFont">View Flights</p>
+                        <img src="viewFlights.jpg" className="cityImage" alt="" />
+                    </div>
+                    <div className="gridDiv" style={{"display":"inline-block"}} onClick={()=>{
                         navigate("/ViewPayments")
-                    }}>View Payments</p>
+                    }}>
+                        <p className="logoFont">View Payments</p>
+                        <img src="viewPayments.jpg" className="cityImage" alt="" />
+                    </div>
                 </div>
             </div>
         </>
